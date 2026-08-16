ac2a7ada4ab0c5dcdcd78e4a0d5b70c3b289d900
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-08-16 22:03:06 +0530

    Create count.sql

---
+++ intermediate/count.sql
@@ -0,0 +1,10 @@
+-- Here's the 5 SQL Aggregate Functions:
+
+-- SUM adds together all the values in a particular column.
+-- MIN returns the lowest value in a particular column
+-- MAX returns the highest value in a particular column
+-- AVG calculates the average of a group of selected values.
+-- COUNT counts how many rows are in a particular column.
+
+SELECT COUNT(*) 
+FROM pharmacy_sales;
