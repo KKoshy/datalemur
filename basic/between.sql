3dd3c12e1a1c8edb8043ef8d13f225575d4e434f
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-08-16 20:26:44 +0530

    Add comment about BETWEEN operator inclusivity
    Added a comment explaining the inclusivity of the BETWEEN operator in SQL.

--- basic/between.sql
+++ basic/between.sql
@@ -1,3 +1,5 @@
+-- In SQL, the BETWEEN operator's range is inclusive. That means both the start and end values of the range are included.
+
 SELECT manufacturer, drug, units_sold
 FROM pharmacy_sales
 WHERE units_sold BETWEEN 100000 AND 105000
