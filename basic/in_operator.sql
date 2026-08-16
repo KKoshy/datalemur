1f897f21d68900af979a1431dde62faaca302e90
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-08-16 20:30:10 +0530

    Document IN operator usage in SQL query
    Added comments explaining the IN operator usage in SQL.

--- basic/in_operator.sql
+++ basic/in_operator.sql
@@ -1,3 +1,6 @@
+-- The IN operator allows us to specify multiple values in a single line's WHERE clause, 
+-- instead of the more tedious approach of having to use multiple OR conditions to filter for multiple values.
+
 SELECT manufacturer, drug, units_sold 
 FROM pharmacy_sales
 WHERE units_sold NOT BETWEEN 55000 AND 550000
