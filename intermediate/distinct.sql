fb17d992abc5c9e8630c98ac23478d31f10f8769
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-08-16 22:44:28 +0530

    Enhance distinct.sql with comments and examples
    Added comments explaining the use of the DISTINCT SQL command and provided an example query.

--- intermediate/distinct.sql
+++ intermediate/distinct.sql
@@ -1,4 +1,16 @@
--- The DISTINCT SQL command is used in conjunction with the SELECT statement to return only distinct (different) values. 
+-- The DISTINCT SQL command is used in conjunction with the SELECT statement to return only distinct (different) values.
+
+-- DISTINCT With Two Columns
+-- If you include two (or more) columns in a SELECT DISTINCT clause, your results will contain all of the unique pairs 
+-- of those two columns.
+
+-- For example, imagine you worked at stock trading app Robinhood and had access to their trades dataset. 
+-- Here's a SQL query that uses DISTINCT on two columns – user_id's and trade statuses:
+
+-- SELECT DISTINCT user_id, status
+-- FROM trades
+-- ORDER BY user_id;
+-- Note: You only need to include DISTINCT once in your SELECT clause—you do not need to add it for each column name.
 
 SELECT category, COUNT(DISTINCT product) 
 FROM product_spend
