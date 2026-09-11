40377c938f5504b56eb49549ebbecc3455910a76
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-09-11 22:23:49 +0530

    Update 12_rank_with_case.sql

--- advanced/12_rank_with_case.sql
+++ advanced/12_rank_with_case.sql
@@ -6,31 +6,19 @@ WITH MeasurementInfo AS (
             ORDER BY measurement_time
         ) AS row_number,
         measurement_value,
-        measurement_time
-    FROM measurements
-),
-
-NewData AS (
-    SELECT
-        measurement_id,
-        CASE
-            WHEN row_number % 2 = 0 THEN 'even'
-            ELSE 'odd'
-        END AS category,
-        measurement_value,
         measurement_time::DATE AS measurement_day
-    FROM MeasurementInfo
+    FROM measurements
 )
 
 SELECT measurement_day,
        SUM(CASE
-            WHEN category='odd' THEN measurement_value
+            WHEN row_number%2=1 THEN measurement_value
             ELSE 0
            END) AS odd_sum,
        SUM(CASE
-            WHEN category='even' THEN measurement_value
+            WHEN row_number%2=0 THEN measurement_value
             ELSE 0
            END) AS even_sum
-FROM NewData
+FROM MeasurementInfo
 GROUP BY measurement_day
 ORDER BY measurement_day;
